4d59a0b9bccb859437f54b4010566a44f88cd555
archblossom <wjstndus1210@naver.com>
2025-11-20 01:08:30 +0900

    update minor

--- decoders/subtitle_color_loss.py
+++ decoders/subtitle_color_loss.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
-import cv2
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import cv2
+import numpy as np
 
 
 class SubtitleColorConsistencyLoss(nn.Module):
